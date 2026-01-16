ddd8424618d2c28bf02cce85aba68e2dd0339f3c
Fahad <faahadpsit@gmail.com>
2026-01-17 02:02:28 +0530

    updates

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Fahad",
+    author_email="2k22.cse.2211260@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
